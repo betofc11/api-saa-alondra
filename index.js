28f3000fdb76f2cd4e5607253e16aea08f2f09d7
betofc11 <60046663+betofc11@users.noreply.github.com>
2022-01-14 06:11:32 -0600

    Allow headers

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ const app = express();
 // HABILITAR BODYPARSER 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+        'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
     next();
 })
 app.use(express.json());
